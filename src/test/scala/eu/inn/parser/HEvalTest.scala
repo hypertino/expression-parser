e8d2d72e9a569e18af65a7b1e5bfeae669ee877a
alexey olenev <aleksey.olenev@inn.ru>
2016-08-12 18:16:52 +0300

    fix test

--- src/test/scala/eu/inn/parser/HEvalTest.scala
+++ src/test/scala/eu/inn/parser/HEvalTest.scala
@@ -9,41 +9,41 @@ class HEvalTest extends FreeSpec with Matchers {
   "HEval" - {
     "math" in {
       val result = HEval("4*5+3*2")
-      result shouldBe Number(26)
+      result.get shouldBe Number(26)
     }
 
     "math with variables" in {
       val context = ObjV("x" → 100)
       val result = HEval("4*x+3*2", context)
-      result shouldBe Number(406)
+      result.get shouldBe Number(406)
     }
 
     "math with variables that doesn't exists" in {
       intercept[EvalEntityNotFound]{
-        HEval("4*x+3*2")
+        HEval("4*x+3*2").get
       }
     }
 
     "math with inner variables" in {
       val context = ObjV("data" → ObjV("x" → 10))
       val result = HEval.apply("4*data.x+1*2", context)
-      result shouldBe Number(42)
+      result.get shouldBe Number(42)
     }
 
     "bool logic" in {
-      HEval("10 > 3") shouldBe True
-      HEval("5 > 6") shouldBe False
-      HEval("[5,6,7] has 6") shouldBe True
-      HEval("[5,6,7] has [6,8]") shouldBe False
-      HEval("[5,6,7] has [6,7]") shouldBe True
-      HEval(""""127.0.0.0/24" has "127.0.0.1"""") shouldBe True
-      HEval(""""127.0.0.0/24" has "126.0.0.1"""") shouldBe False
-      HEval(""""127.0.0.1 - 128.0.0.1" has "127.0.0.1"""") shouldBe True
-      HEval(""""127.0.0.1 - 128.0.0.1" has "126.0.0.1"""") shouldBe False
+      HEval("10 > 3").get shouldBe True
+      HEval("5 > 6").get shouldBe False
+      HEval("[5,6,7] has 6").get shouldBe True
+      HEval("[5,6,7] has [6,8]").get shouldBe False
+      HEval("[5,6,7] has [6,7]").get shouldBe True
+      HEval(""""127.0.0.0/24" has "127.0.0.1"""").get shouldBe True
+      HEval(""""127.0.0.0/24" has "126.0.0.1"""").get shouldBe False
+      HEval(""""127.0.0.1 - 128.0.0.1" has "127.0.0.1"""").get shouldBe True
+      HEval(""""127.0.0.1 - 128.0.0.1" has "126.0.0.1"""").get shouldBe False
 
       HEval("""
         "hello" has "el"
-      """) shouldBe True
+      """).get shouldBe True
     }
 
     "regex like test" in {
@@ -57,33 +57,33 @@ class HEvalTest extends FreeSpec with Matchers {
     }
 
     "functions test" in {
-      HEval("!false") shouldBe True
-      HEval("""isEmpty("")""") shouldBe True
-      // HEval("""!isEmpty("")""") shouldBe False //todo: uncomment when fixed
-      HEval("""isEmpty("b")""") shouldBe False
-      HEval("""isExists(a)""") shouldBe False
+      HEval("!false").get shouldBe True
+      HEval("""isEmpty("")""").get shouldBe True
+      // HEval("""!isEmpty("")""").get shouldBe False //todo: uncomment when fixed
+      HEval("""isEmpty("b")""").get shouldBe False
+      HEval("""isExists(a)""").get shouldBe False
     }
 
     "text functions" in {
       HEval("""
         split("a,b,c", ",")
-      """) shouldBe LstV("a","b","c")
+      """).get shouldBe LstV("a","b","c")
 
       HEval("""
         indexOf("abc","b")
-      """) shouldBe Number(1)
+      """).get shouldBe Number(1)
 
       HEval("""
         substr("abc",1)
-      """) shouldBe Text("bc")
+      """).get shouldBe Text("bc")
 
       HEval("""
         substr("abc",1,2)
-      """) shouldBe Text("b")
+      """).get shouldBe Text("b")
     }
 
     "apply test" in {
-      HEval("""["a","b","c"](1)""") shouldBe Text("b")
+      HEval("""["a","b","c"](1)""").get shouldBe Text("b")
     }
 
     "custom function test" in {
@@ -93,7 +93,7 @@ class HEvalTest extends FreeSpec with Matchers {
         }
       }
 
-      HEval("""pow(2,8)""", context) shouldBe Number(256)
+      HEval("""pow(2,8)""", context).get shouldBe Number(256)
     }
   }
 }
--- src/test/scala/eu/inn/parser/HParserTest.scala
+++ src/test/scala/eu/inn/parser/HParserTest.scala
@@ -9,59 +9,59 @@ class HParserTest extends FreeSpec with Matchers {
 
   "HParser" - {
     "numbers" in {
-      HParser("1234") shouldBe Constant(bn.Number(1234))
-      HParser("0.123") shouldBe Constant(bn.Number(0.123))
-      HParser("1e3") shouldBe Constant(bn.Number(1e3))
-      HParser("12.8E3") shouldBe Constant(bn.Number(12.8E3))
-      HParser("0xFF") shouldBe Constant(bn.Number(0xFF))
-      HParser("\n0X12") shouldBe Constant(bn.Number(0x12))
+      HParser("1234").get shouldBe Constant(bn.Number(1234))
+      HParser("0.123").get shouldBe Constant(bn.Number(0.123))
+      HParser("1e3").get shouldBe Constant(bn.Number(1e3))
+      HParser("12.8E3").get shouldBe Constant(bn.Number(12.8E3))
+      HParser("0xFF").get shouldBe Constant(bn.Number(0xFF))
+      HParser("\n0X12").get shouldBe Constant(bn.Number(0x12))
     }
 
     "bool" in {
-      HParser("true") shouldBe Constant(bn.True)
-      HParser("\nfalse") shouldBe Constant(bn.False)
+      HParser("true").get shouldBe Constant(bn.True)
+      HParser("\nfalse").get shouldBe Constant(bn.False)
     }
 
     "identifier" in {
-      HParser("abc") shouldBe Identifier("abc")
-      HParser("abc.xyz") shouldBe Identifier(Seq("abc","xyz"))
-      HParser("abc.xyz.klm") shouldBe Identifier(Seq("abc","xyz","klm"))
-      HParser("`with space`") shouldBe Identifier("with space")
-      HParser("test.`with space`") shouldBe Identifier(Seq("test","with space"))
-      HParser("`with``backtick`") shouldBe Identifier("with`backtick")
+      HParser("abc").get shouldBe Identifier("abc")
+      HParser("abc.xyz").get shouldBe Identifier(Seq("abc","xyz"))
+      HParser("abc.xyz.klm").get shouldBe Identifier(Seq("abc","xyz","klm"))
+      HParser("`with space`").get shouldBe Identifier("with space")
+      HParser("test.`with space`").get shouldBe Identifier(Seq("test","with space"))
+      HParser("`with``backtick`").get shouldBe Identifier("with`backtick")
     }
 
     "unary expressions" in {
-      HParser("!abc") shouldBe UnaryOperation(Identifier("!"), Identifier("abc"))
-      HParser("!abc.xyz") shouldBe UnaryOperation(Identifier("!"), Identifier(Seq("abc","xyz")))
-      HParser("!true") shouldBe UnaryOperation(Identifier("!"), Constant(bn.True))
+      HParser("!abc").get shouldBe UnaryOperation(Identifier("!"), Identifier("abc"))
+      HParser("!abc.xyz").get shouldBe UnaryOperation(Identifier("!"), Identifier(Seq("abc","xyz")))
+      HParser("!true").get shouldBe UnaryOperation(Identifier("!"), Constant(bn.True))
     }
 
     "binary expressions" in {
-      HParser("1+2") shouldBe BinaryOperation(Constant(bn.Number(1)), Identifier("+"), Constant(bn.Number(2)))
-      HParser("1+-2") shouldBe BinaryOperation(Constant(bn.Number(1)), Identifier("+"), Constant(bn.Number(-2)))
-      HParser("1+2-3") shouldBe BinaryOperation(
+      HParser("1+2").get shouldBe BinaryOperation(Constant(bn.Number(1)), Identifier("+"), Constant(bn.Number(2)))
+      HParser("1+-2").get shouldBe BinaryOperation(Constant(bn.Number(1)), Identifier("+"), Constant(bn.Number(-2)))
+      HParser("1+2-3").get shouldBe BinaryOperation(
         BinaryOperation(Constant(bn.Number(1)), Identifier("+"),Constant(bn.Number(2))),
         Identifier("-"), Constant(bn.Number(3))
       )
 
-      HParser("1 has 2") shouldBe BinaryOperation(Constant(bn.Number(1)), Identifier("has"), Constant(bn.Number(2)))
-      HParser("1 has not 2") shouldBe BinaryOperation(Constant(bn.Number(1)), Identifier("has not"), Constant(bn.Number(2)))
-      HParser("1 has \t not 2") shouldBe BinaryOperation(Constant(bn.Number(1)), Identifier("has not"), Constant(bn.Number(2)))
+      HParser("1 has 2").get shouldBe BinaryOperation(Constant(bn.Number(1)), Identifier("has"), Constant(bn.Number(2)))
+      HParser("1 has not 2").get shouldBe BinaryOperation(Constant(bn.Number(1)), Identifier("has not"), Constant(bn.Number(2)))
+      HParser("1 has \t not 2").get shouldBe BinaryOperation(Constant(bn.Number(1)), Identifier("has not"), Constant(bn.Number(2)))
 
-      HParser("x <= 2") shouldBe BinaryOperation(Identifier("x"), Identifier("<="), Constant(bn.Number(2)))
-      HParser("2 >= y") shouldBe BinaryOperation(Constant(bn.Number(2)), Identifier(">="), Identifier("y"))
+      HParser("x <= 2").get shouldBe BinaryOperation(Identifier("x"), Identifier("<="), Constant(bn.Number(2)))
+      HParser("2 >= y").get shouldBe BinaryOperation(Constant(bn.Number(2)), Identifier(">="), Identifier("y"))
     }
 
     "binary operator precedence" in {
-      HParser("5+10*3") shouldBe BinaryOperation(Constant(bn.Number(5)), Identifier("+"),
+      HParser("5+10*3").get shouldBe BinaryOperation(Constant(bn.Number(5)), Identifier("+"),
         BinaryOperation(Constant(bn.Number(10)), Identifier("*"), Constant(bn.Number(3)))
       )
-      HParser("5*10+3") shouldBe BinaryOperation(
+      HParser("5*10+3").get shouldBe BinaryOperation(
         BinaryOperation(Constant(bn.Number(5)), Identifier("*"), Constant(bn.Number(10))),
         Identifier("+"), Constant(bn.Number(3))
       )
-      HParser("5*10 - 3/4") shouldBe BinaryOperation(
+      HParser("5*10 - 3/4").get shouldBe BinaryOperation(
         BinaryOperation(Constant(bn.Number(5)), Identifier("*"), Constant(bn.Number(10))),
         Identifier("-"),
         BinaryOperation(Constant(bn.Number(3)), Identifier("/"), Constant(bn.Number(4)))
@@ -69,32 +69,32 @@ class HParserTest extends FreeSpec with Matchers {
     }
 
     "parens expression" in {
-      HParser("\n(5+10)*3") shouldBe BinaryOperation(
+      HParser("\n(5+10)*3").get shouldBe BinaryOperation(
         BinaryOperation(Constant(bn.Number(5)), Identifier("+"), Constant(bn.Number(10))),
         Identifier("*"), Constant(bn.Number(3))
       )
     }
 
     "function" in {
-      HParser("test()") shouldBe Function(Identifier("test"),
+      HParser("test()").get shouldBe Function(Identifier("test"),
         Seq.empty
       )
-      HParser("\ntest()\n") shouldBe Function(Identifier("test"),
+      HParser("\ntest()\n").get shouldBe Function(Identifier("test"),
         Seq.empty
       )
-      HParser("test(1,2,3)") shouldBe Function(Identifier("test"),
+      HParser("test(1,2,3)").get shouldBe Function(Identifier("test"),
         Seq(
           Constant(bn.Number(1)),Constant(bn.Number(2)),Constant(bn.Number(3))
         )
       )
-      HParser("test(5+10)") shouldBe Function(Identifier("test"),
+      HParser("test(5+10)").get shouldBe Function(Identifier("test"),
         Seq(BinaryOperation(Constant(bn.Number(5)), Identifier("+"), Constant(bn.Number(10))))
       )
     }
 
     "string tests" in {
-      HParser("\"\\t\"") shouldBe Constant(Text("\t"))
-      HParser("\"\\u000A\"") shouldBe Constant(Text("\n"))
+      HParser("\"\\t\"").get shouldBe Constant(Text("\t"))
+      HParser("\"\\u000A\"").get shouldBe Constant(Text("\n"))
     }
   }
 }
